ff4171cd3ea9b56feb4b0fb21fbbf5997350e95d
SAHEB GUCHHAIT <guchhait.saheb10@gmail.com>
2024-12-23 11:48:49 +0530

    Initial commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[app-warning-alert]',
+  templateUrl: './warning-alert.component.html',
+  // styleUrl: './warning-alert.component.css'
+  styles: [
+    `
+      p {
+        color: red;
+        font-weight: bold;
+        background-color: #f8d7da;
+        padding: 10px;
+        border: 1px solid red;
+        border-radius: 5px;
+      }
+    `,
+  ],
+})
+export class WarningAlertComponent {}
